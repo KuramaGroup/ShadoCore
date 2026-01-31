572067e88261ecc21099af49dd56c0b85c67b345
Kittnz <315169700@qq.com>
2026-01-31 13:09:54 +0800

    DB/Misc : Additions to Valley Of The Four Winds Quest "Great Minds Drink Alike"

---
+++ sql/updates/world/202601311308_world.sql
@@ -0,0 +1,6 @@
+DELETE FROM waypoints WHERE entry = 56573;
+INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `position_z`, `point_comment`) VALUES (56573, 1, 33.444, -138.862, 190.664, 'Mudmug');
+INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `position_z`, `point_comment`) VALUES (56573, 2, 7.099, -156.612, 177.957, 'Mudmug');
+INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `position_z`, `point_comment`) VALUES (56573, 3, -64.396, -69.713, 161.586, 'Mudmug');
+INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `position_z`, `point_comment`) VALUES (56573, 4, -55.762, -33, 154.148, 'Mudmug');
+INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `position_z`, `point_comment`) VALUES (56573, 5, -39.513, -16.635, 154.515, 'Mudmug');
